e9d70b13110f67d0421534aed6db4c5ec13c6ec5
Dmytro Polishchuk <dpolishchuk@scalefree.com>
2024-03-24 23:56:58 +0100

    Add macro create_index

---
+++ macros/create_index.sql
@@ -0,0 +1,21 @@
+{% macro create_index(index_name, table, columns, unique) -%}
+    
+    {%- set ns=namespace(column_list_print="") -%}
+    {%- if columns is string -%}
+        {%- set column_list = [columns] -%}
+    {%- else %}
+        {%- set column_list = columns -%}
+    {%- endif -%}
+
+    {%- for column in column_list -%}
+        {%- set ns.column_list_print = ns.column_list_print ~ column ~ (', ' if not loop.last else '') -%}
+    {%- endfor -%}
+
+    {%- set query -%}
+    CREATE{%- if unique %} UNIQUE{% endif %} INDEX IF NOT EXISTS {{ index_name }} ON {{ table }} ( {{ ns.column_list_print }} );
+    {%- endset -%}
+
+    {%- do log(query, info=true) -%}
+    {%- do run_query('BEGIN;\n'~ query ~ '\nCOMMIT;') -%}
+
+{%- endmacro %}
