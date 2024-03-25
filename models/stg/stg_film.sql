0100933baf62145160eaa757eea09602549959ae
Dmytro Polishchuk <dpolishchuk@scalefree.com>
2024-03-25 23:31:14 +0100

    stg_film: change foreign key constraing and fix commented test input

--- models/stg/stg_film.sql
+++ models/stg/stg_film.sql
@@ -23,20 +23,18 @@ from {{ source('dvdrental', 'film') }}
 where last_update >
         ( select coalesce(max(last_update), '1900-01-01') from {{ this }} )
 {% endif %}
-
 /*
 union all
-
 select
-     9999 as film_id
-    ,'Test film' title
-    ,'Test description' description
-    ,'1900' release_year
-    ,1 as language_id -- from 1 to 6
-    ,10 as rental_duration
-    ,9.99 rental_rate
-    ,200 as length
-    ,99.99 as replacement_cost
-    ,'TEST-18' as rating
+     9999::integer as film_id
+    ,'Test film'::varchar(255) title
+    ,'Test description'::text description
+    ,1899::integer release_year --<=1900
+    ,6::smallint as language_id -- from 1 to 6
+    ,10::smallint as rental_duration
+    ,9.99::numeric(4,2) rental_rate
+    ,200::smallint as length
+    ,99.99::numeric(5,2) as replacement_cost
+    ,'TEST-18'::varchar(10) as rating
     ,current_timestamp::timestamp as last_update
-*/
+  */
